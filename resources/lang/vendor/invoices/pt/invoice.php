6d711637f4747c2dda1a7d951193324a12246eac
Afonso <afonso9980@gmail.com>
2021-06-22 13:06:46 +0100

    Installed invoice library and setted it's classes

---
+++ resources/lang/vendor/invoices/pt/invoice.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'serial'          => 'Numéro Serial.',
+    'date'            => 'Data da Fatura',
+    'seller'          => 'Vendedor',
+    'buyer'           => 'Comprador',
+    'address'         => 'Endereço',
+    'code'            => 'Código',
+    'vat'             => 'VAT',
+    'phone'           => 'Telemóvel',
+    'description'     => 'Descrição',
+    'units'           => 'Unidades',
+    'quantity'        => 'Quantidade',
+    'price'           => 'Preço',
+    'discount'        => 'Desconto',
+    'tax'             => 'IVA',
+    'sub_total'       => 'Sub total',
+    'total_discount'  => 'Total de Desconto',
+    'taxable_amount'  => 'IVA amount',
+    'total_taxes'     => 'IVA total',
+    'tax_rate'        => 'Tax rate',
+    'total_amount'    => 'Preço Total',
+    'pay_until'       => 'Por favor, pague até',
+    'amount_in_words' => 'Amount in words',
+    'notes'           => 'Notas',
+    'shipping'        => 'Envio',
+
+];
